43cbf6671096b519fe0fdbaa04f793a4ed606641
claudiocarvalhoo <cco2@cin.ufpe.br>
2019-09-14 21:39:20 -0300

    feat(MainActivity): add function to download xml from link

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,11 +2,21 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        downloadXML()
+    }
+
+    fun downloadXML() {
+        doAsync {
+            val feed = URL("https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml").readText()
+            val itemFeeds = Parser.parse(feed)
+        }
     }
 }
